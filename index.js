5db55a0b961c64f76f5edc92241c5cc176129c54
Sanju Shah <sanjushahgupta@gmail.com>
2023-06-23 09:37:20 +0200

    refactor: endpoints

--- index.js
+++ index.js
@@ -7,44 +7,42 @@ const express = require('express'),
     path = require('path'),
     accessLogStream = fs.createWriteStream(path.join('log.txt'), { flags: 'a' });
 
-app.use(bodyparser.json());
-
-let users = [{ id: 1, name: "Sanju", favoritesMovies: [] }, { id: 2, name: "Sanjufsd", favoritesMovies: [] }];
+let users = [{ id: 1, name: "Sanju", favoritesMovies: [] }, { id: 2, name: "Amit", favoritesMovies: [] }];
 let moviesInfo = [
-    { Id: "1", Title: "Titanic", Genre: { Name: "Comedy", description: "  entertaining, exciting" }, Director: { Name: "James Cameron", Birth: "1990" }, imageurl: "#" },
-    { Id: "2", Title: "Friends", Genre: { Name: "Drama", description: "  entertaining, exciting" }, Director: { Name: "Jafar Panahi", Birth: "1970" }, imageurl: "#" },
-    { Id: "3", Title: "Green Book", Genre: { Name: "Fantasy", description: " entertaining, exciting" }, Director: { Name: "Norman Panama", Birth: "1985" }, imageurl: "#" },
-    { Id: "4", Title: "Bojo Horseman", Genre: { Name: "Action", description: " entertaining, exciting" }, Director: { Name: "Alan J. Pakula", Birth: "1995" }, imageurl: "#" },
+    { Id: "1", Title: "Titanic", Genre: { Name: "Comedy", description: "entertaining, exciting" }, Director: { Name: "James Cameron", Birth: "1990" }, imageurl: "#" },
+    { Id: "2", Title: "Friends", Genre: { Name: "Drama", description: "entertaining, exciting" }, Director: { Name: "Jafar Panahi", Birth: "1970" }, imageurl: "#" },
+    { Id: "3", Title: "Book", Genre: { Name: "Fantasy", description: "entertaining, exciting" }, Director: { Name: "Norman Panama", Birth: "1985" }, imageurl: "#" },
+    { Id: "4", Title: "Bojo Horseman", Genre: { Name: "Action", description: "entertaining, exciting" }, Director: { Name: "Alan J. Pakula", Birth: "1995" }, imageurl: "#" },
     { Id: "5", Title: "BlackSea", Genre: { Name: "Horror", description: "entertaining, exciting" }, Director: { Name: "P.Padmarajan", Birth: "1955" }, imageurl: "#" }]
 
-
+app.use(bodyparser.json());
 
 app.get('/movies', (req, res) => {
-    res.json(moviesInfo);
+    return res.json(moviesInfo);
 })
 
 app.get('/movies/:title', (req, res) => {
-
     const movie = moviesInfo.find(movie =>
         movie.Title === req.params.title
     );
     if (movie) {
-        res.status(200).json(movie);
-    } else {
-        res.status(400).send('Sorry, we cannot find such movie.');
+        return res.status(200).json(movie);
     }
+    return res.status(400).send('Sorry, we cannot find such movie.');
 })
 
-app.get('/movie/genre/:name', (req, res) => {
+
+app.get('/movies/genre/:name', (req, res) => {
     const movieGenre = moviesInfo.find(movie =>
         movie.Genre.Name === req.params.name
     ).Genre;
     if (movieGenre) {
-        res.status(200).json(movieGenre);
-    } else {
-        res.status(400).send('Sorry, we cannot find such movie.');
+        return res.status(200).json(movieGenre);
     }
+    return res.status(400).send('Sorry, we cannot find such genre.');
+
 })
+
 
 app.get('/movies/directors/:name', (req, res) => {
     const directorDetails = moviesInfo.find(movie =>
@@ -53,8 +51,9 @@ app.get('/movies/directors/:name', (req, res) => {
     if (directorDetails) {
         return res.status(200).json(directorDetails);
     }
-    return res.status(400).send('Sorry, we cannot find such movie.');
+    return res.status(400).send('Sorry, we cannot find director details.');
 })
+
 
 app.post('/register', (req, res) => {
     const newUser = req.body;
@@ -64,24 +63,25 @@ app.post('/register', (req, res) => {
         users.push(newUser);
         return res.status(200).json(newUser)
     }
-    return res.status(400).send("Check your credentials")
+    return res.status(400).send("Sorry,Unable to register.please check your credentials")
 })
 
-
 app.put('/user/:id', (req, res) => {
-    const updateUser = req.body;
-    const user = users.find(user => user.id == req.params.id);
+    const updateUser = req.body,
+        user = users.find(user => user.id == req.params.id);
+
     if (user) {
         user.name = updateUser.name;
-        return res.status(200).send("Username has been updated.");
+        return res.status(200).json(user);
     }
-    return res.status(400).send("Sorry, username can be updated. Try again")
+    return res.status(400).send("Sorry, username is not updated. Try again")
 })
 
 app.post('/addfab/:userId/:movieTitle', (req, res) => {
-    const userId = req.params.userId;
-    const movieTitle = req.params.movieTitle;
-    const user = users.find(user => user.id == userId);
+    const userId = req.params.userId,
+        movieTitle = req.params.movieTitle,
+        user = users.find(user => user.id == userId);
+
     if (user) {
         user.favoritesMovies.push(movieTitle)
         return res.status(200).send("Movie is added to favourite list.")
@@ -89,24 +89,29 @@ app.post('/addfab/:userId/:movieTitle', (req, res) => {
     return res.status(400).send("Sorry! Movie is not added to favourite list.")
 })
 
+
 app.delete('/deletefab/:userId/:movieTitle', (req, res) => {
-    const userId = req.params.userId;
-    const user = users.find(user => user.id == userId);
+    const userId = req.params.userId,
+        movieTitle = req.params.movieTitle,
+        user = users.find(user => user.id == userId);
+
     if (user) {
-        user.favoritesMovies = user.favoritesMovies.filter(movie => movie.Title !== req.params.movieTitle)
-        return res.status(200).send("Movie is delete from favourite list.")
+        user.favoritesMovies = user.favoritesMovies.filter(movie => movie.Title !== movieTitle)
+        return res.status(200).send("Movie is deleted from favourite list.")
     }
-    return res.status(400).send("Sorry! try again")
+    return res.status(400).send("Sorry! Movie is not delete from favourite list.")
 })
 
 app.delete('/deleteUser/:id', (req, res) => {
     const userId = req.params.id;
+
     users = users.filter(user => user.id != userId);
     return res.status(200).send("Account deleted");
 })
 
 app.use(express.static('public'));
 app.use(morgan('combined', { stream: accessLogStream }));
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send("Something went wrong");
