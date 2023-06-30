7c8c1b8f2e458387ff75b13d972eda197670b766
Sanju Shah <sanjushahgupta@gmail.com>
2023-06-30 15:20:40 +0200

    feat: implement CORS, password hasing and input validation

--- index.js
+++ index.js
@@ -231,6 +231,7 @@ app.use((err, req, res, next) => {
     res.status(500).send("error" + err);
 })
 
-app.listen(8080, () => {
-    console.log("App is listening on port 8080.");
-}) 
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+    console.log('App is listening in ' + port);
+});
